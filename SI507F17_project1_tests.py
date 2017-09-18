2176152a04cdb518b7787ea977aaae5560b9d123
Innocent Obi <icn5@georgetown.edu>
2017-09-18 11:21:05 -0400

    version that is PEP8 compliant

--- SI507F17_project1_tests.py
+++ SI507F17_project1_tests.py
@@ -4,39 +4,42 @@ import unittest
 from SI507F17_project1_cards import *
 
 # Write your unit tests to test the cards code here.
-# You should test to ensure that everything explained in the code description file works as that file says.
+# You should test to ensure that everything explained in
+# the code description file works as that file says.
 # If you have correctly written the tests, at least 3 tests should fail.
-# If more than 3 tests fail, it should be because multiple of the test methods address the same problem in the code.
-# You may write as many TestSuite subclasses as you like, but you should try to make these tests well-organized and easy to read the output.
+# If more than 3 tests fail, it should be because multiple of the test
+# methods address the same problem in the code.
+# You may write as many TestSuite subclasses as you like, but you should try
+# to make these tests well-organized and easy to read the output.
 # You should invoke the tests with verbosity=2 (make sure you invoke them!)
 
 class ClassTests(unittest.TestCase):   
     def setUp(self):
         self.card = Card()
-        
+
     def test_Suit_Type(self):
-        self.assertEqual(type(self.card.suit_names), type([]))
-        
+        self.assertEqual(type(self.card.suit_names), type([]))       
+
     def test_SuitContains(self):
         lists = ["Diamonds", "Clubs", "Hearts", "Spades"]
-        self.assertListEqual(lists, self.card.suit_names)
-        
+        self.assertListEqual(lists, self.card.suit_names)       
+
     def test_len(self):
         self.assertTrue(self.card.suit_names)
         self.assertTrue(self.card.rank_levels)
         self.assertTrue(self.card.faces)
-        
+
 #   Rank tests        
     def test_Rank_Type(self):
-        self.assertEqual(type(self.card.rank_levels), type([]))
-        
+        self.assertEqual(type(self.card.rank_levels), type([]))       
+
     def test_vRank_Integer(self):
         for item in self.card.rank_levels:
-            self.assertEqual(type(item), type(1))
-            
+            self.assertEqual(type(item), type(1))           
+
     def test_RankEqualsThirtheen(self):
-        self.assertTrue(len(self.card.rank_levels), 13)
-        
+        self.assertTrue(len(self.card.rank_levels), 13)       
+
     def test_variable_count(self):
         i = 0
         for item in self.card.rank_levels:
@@ -45,24 +48,25 @@ class ClassTests(unittest.TestCase):
             i += 1
 
 #   These are the test for the Faces
+
     def test_faces(self):
-        self.assertEqual(type(self.card.faces), type({}))
-        
+        self.assertEqual(type(self.card.faces), type({}))       
+
     def test_FacesKey(self):
-        self.assertTrue(type(self.card.faces.keys()), int)
-        
+        self.assertTrue(type(self.card.faces.keys()), int)       
+
     def test_FacesValue(self):
         self.assertTrue(type(self.card.faces.values()), str)
 
     def test_FaceAceOnePair(self):
-        self.assertTrue(self.card.faces[1], "Ace")
-        
+        self.assertTrue(self.card.faces[1], "Ace")       
+
     def test_FaceJackEleven(self):
-        self.assertTrue(self.card.faces[11], "Jack")
-        
+        self.assertTrue(self.card.faces[11], "Jack")       
+
     def test_FaceQueenTweleve(self):
-        self.assertTrue(self.card.faces[12], "Queen")
-        
+        self.assertTrue(self.card.faces[12], "Queen")       
+
     def test_FaceKingThirteen(self):
         self.assertTrue(self.card.faces[13], "King")
 
@@ -83,8 +87,8 @@ class ClassTests(unittest.TestCase):
         elif self.card.suit == "Spades":
             pass
         else:
-            self.assertRaises(ValueError)
-                
+            self.assertRaises(ValueError)               
+
     def test_SelfRank(self):
         if self.card.rank_levels == 11:
             self.assertEqual(self.card.rank, "Jack")
@@ -95,11 +99,11 @@ class ClassTests(unittest.TestCase):
         elif self.card.rank_levels == 1:
             self.assertEqual(self.card.rank, "Ace")
         else:
-            pass
-        
+            pass       
+
     def test_SelfRanNum(self):
-        self.assertEqual(type(self.card.rank_num), int)
-        
+        self.assertEqual(type(self.card.rank_num), int)       
+
     def test_SelfTanNumRange(self):
         self.assertIn(self.card.rank_num, range(1,14))
 
@@ -116,8 +120,9 @@ class ClassTests(unittest.TestCase):
         Test = Card(3, 13)
         self.assertEqual(str(Test), "Ace of Spades")
 
+
 class DeckTest(unittest.TestCase):
-    
+
     def setUp(self):
             self.deck = Deck()
             self.card = Card()
@@ -126,8 +131,8 @@ class DeckTest(unittest.TestCase):
         self.assertRaises(TypeError, Deck(0))
 
     def test_DeckCreatsList(self):
-        self.assertEqual(type(self.deck.cards), type([]))
-        
+        self.assertEqual(type(self.deck.cards), type([]))     
+
     def test_cardsInstanceinDeck(self):
         for item in self.deck.cards:
             self.assertIsInstance(item, Card)
@@ -142,23 +147,18 @@ class DeckTest(unittest.TestCase):
 #        print(type(Deck))
 #        print(type(Card))
 #        print(type(self.deck.cards))
-        self.assertIsInstance(self.deck, Deck)
-        
+        self.assertIsInstance(self.deck, Deck)       
+
     def test_DeckHasAll(self):
-        for i in range(13):
-            
+        for i in range(13):           
             if self.deck.cards[i].rank == "Ace":
-                self.assertIn(self.deck.cards[i].rank_num, range(1,14))
-                
+                self.assertIn(self.deck.cards[i].rank_num, range(1,14))               
             elif self.deck.cards[i].rank == "Jack":
                 self.assertIn(self.deck.cards[i].rank_num, range(1,14))
-
             elif self.deck.cards[i].rank == "Queen":
                 self.assertIn(self.deck.cards[i].rank_num, range(1,14))
-
             elif self.deck.cards[i].rank == "King":
-                self.assertIn(self.deck.cards[i].rank_num, range(1,14))
-                
+                self.assertIn(self.deck.cards[i].rank_num, range(1,14))               
             else:
                 self.assertIn(self.deck.cards[i].rank, range(1,14))
 
@@ -183,15 +183,15 @@ class DeckTest(unittest.TestCase):
     def test_ifitprintsAce(self):
         deck = str(self.deck)
         split = deck.split("\n")
-        self.assertEqual(52, len(split))
-        
+        self.assertEqual(52, len(split))       
+
     def test_equivalenceinTest(self): # BUG2 (due to the same reason maybe?
-        self.assertEqual(str(self.deck.cards[0]),"Ace of Diamonds")
-                         
+        self.assertEqual(str(self.deck.cards[0]),"Ace of Diamonds")                        
+
     def test_typeofPop_cardDefault(self):
         card = self.deck.pop_card()
-#        print(str(card))
-        
+#        print(str(card))       
+
     def test_typeofPop_cardInteger(self):
         card = self.deck.pop_card(2)
 #       print(str(card))
@@ -228,75 +228,44 @@ class DeckTest(unittest.TestCase):
         splitThree = listingThree.split("\n")
 #       print(len(splitThree))
 #       print(len(splitOne))
-
         self.assertEqual(len(splitThree), len(splitOne))
 
     def tests_thatEveryCardIsInDeck(self):
         listingTwo = str(self.deck)
         splitTwo = listingTwo.split("\n")
-
         for i in range(4):
             for n in range(1,14):
                 self.card = Card(i,n)
                 self.assertIn(str(self.card), splitTwo)
 
     def test_method_ifCardOut(self):
-#       for i in range(52):
-#           self.assertIsInstance(self.card, Card) 
-        self.card = Card(3,13)
-#        print(str(self.card))       
+        self.card = Card(3,13)      
         listingThree = str(self.deck)
         splitThree = listingThree.split("\n")
         popped_card = self.deck.pop_card()
-#        print(str(popped_card))
-#        print(len(self.deck.cards))
         self.assertEqual(len(splitThree), len(self.deck.cards))
 
-    def test_method_ifCardOut(self):
-#       for i in range(52):
-#           self.assertIsInstance(self.card, Card)
-        
-        self.card = Card(1,13)
-#        print(str(self.card))
-        
+    def test_method_ifCardOut(self):      
+        self.card = Card(1,13)      
         listingThree = str(self.deck)
-        splitThree = listingThree.split("\n")
-    
+        splitThree = listingThree.split("\n")   
         popped_card = self.deck.pop_card()
-#        print(str(popped_card))
-#        print(len(self.deck.cards))
-
         self.deck.replace_card(self.card)
-#        print(len(self.deck.cards))
-
         self.assertNotEqual(len(splitThree), len(self.deck.cards))
 
     def test_methodJustFail(self):
         self.card = Card(1,13)
-#        print(len(self.deck.cards))
-
         self.deck.replace_card(self.card)
-#        print(len(self.deck.cards))
-
         self.deck.replace_card(self.card)
-#        print(len(self.deck.cards))
 
     def test_sort(self): #Bug3
-#       self.deck.shuffle()
-#       print(str(self.deck))
-#       self.deck.sort_cards()
-#       print(str(self.deck))
-#       print(str(self.deck))
         self.deck.pop_card()
         lenghtPopped = len(self.deck.cards)
         self.deck.shuffle()
-#       print(len(self.deck.cards))
-#       print(str(self.deck))
         self.deck.sort_cards()
-#       print(str(self.deck))
         lenghtSort = len(self.deck.cards)
         self.assertEqual(lenghtPopped, lenghtSort)       
-        
+
     def test_deal(self): #BUG4
         for i in range(3):
             self.deck.pop_card()
@@ -304,14 +273,13 @@ class DeckTest(unittest.TestCase):
 
     def test_Game(self):
         self.assertEqual(type(play_war_game("TRUE")), type(()))
-        
+
     def test_GameTwo(self):
         Hand = play_war_game("TRUE")
         first = Hand[0]
         second = Hand[1]
         third = Hand[2]
-        self.assertEqual(type(first), str)
-        
+        self.assertEqual(type(first), str) 
         while True:
             if first == "Player":
                 break
@@ -322,14 +290,12 @@ class DeckTest(unittest.TestCase):
             else:
                 self.assertRaises(ValueError)
                 break
-                
         self.assertEqual(type(second), int)
         self.assertEqual(type(third), int)
 
     def test_showSong(self):
         song = show_song()
         self.assertIsInstance(song, helper_functions.Song)
-
-                
+               
 if __name__ == "__main__":
     unittest.main(verbosity=2)
